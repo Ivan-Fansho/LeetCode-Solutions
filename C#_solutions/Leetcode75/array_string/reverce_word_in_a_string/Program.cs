3a52c87185a37212d22f29881c0bde66f1cbfa31
Ivan <ivanaleksandrov98@gmail.com>
2024-09-05 12:53:59 +0300

    adding reverse words in string

--- C#_solutions/Leetcode75/array_string/reverce_word_in_a_string/Program.cs
+++ C#_solutions/Leetcode75/array_string/reverce_word_in_a_string/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿static string ReverseWords(string input)
+{
+    string[] words = input.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+    Array.Reverse(words);
+
+    return string.Join(" ", words).Trim();
+}
+
+string input = "a good   example";
+        
+        // Call the method to reverse words
+string reversed = ReverseWords(input);
+        
+        // Output the result
+Console.WriteLine(reversed);
