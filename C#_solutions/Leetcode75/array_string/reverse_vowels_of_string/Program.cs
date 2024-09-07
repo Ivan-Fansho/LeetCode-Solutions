73f0ddc15b29ac324db3010a8844e2dd3a983750
Ivan <ivanaleksandrov98@gmail.com>
2024-09-07 10:30:00 +0300

    adding reverse vowels

--- C#_solutions/Leetcode75/array_string/reverse_vowels_of_string/Program.cs
+++ C#_solutions/Leetcode75/array_string/reverse_vowels_of_string/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using System.Text;
+
+static string ReverseVowels(string s) 
+{
+        var str = new StringBuilder();
+        var l = new Stack<char>();
+
+        foreach(var i in s){
+            if("aeiouAEIOU".Contains(i))
+                l.Push(i);
+        }
+
+        foreach(var i in s){
+            if("aeiouAEIOU".Contains(i))
+                str.Append(l.Pop());
+            else
+                str.Append(i);
+        }
+        
+        return str.ToString();
+
+}
+
+Console.WriteLine(ReverseVowels("hello"));
+Console.WriteLine(ReverseVowels("leetcode"));
