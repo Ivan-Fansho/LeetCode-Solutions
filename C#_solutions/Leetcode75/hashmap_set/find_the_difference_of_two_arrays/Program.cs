a76ab3e76d1df1d77dfb882878432f2dedee3c5f
Ivan <ivanaleksandrov98@gmail.com>
2024-09-07 10:50:38 +0300

    find difference in two lists

--- C#_solutions/Leetcode75/hashmap_set/find_the_difference_of_two_arrays/Program.cs
+++ C#_solutions/Leetcode75/hashmap_set/find_the_difference_of_two_arrays/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿static IList<IList<int>> FindDifference(int[] nums1, int[] nums2) 
+{
+            HashSet<int> h1 = new HashSet<int>(nums1), h2 = new HashSet<int>(nums2);
+
+        return new List<IList<int>> { 
+            h1.Where(x => !h2.Contains(x)).ToList<int>(), 
+            h2.Where(x => !h1.Contains(x)).ToList<int>() 
+        };
+}
+        
+
